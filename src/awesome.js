d9fe52f7959264b71d591945c44c7f454fff27d7
Saurabh97-dev <saurabhitdeveloper@gmail.com>
2024-12-17 16:00:18 +0530

    Update awesome.js

--- src/awesome.js
+++ src/awesome.js
@@ -1,6 +1,6 @@
 export default {
   sauce: function(){
-    alert('your crap websites is now been awesome sauced');
+    alert('your crap website is now been awesome sauced');
     console.log('your crap websites website is now awesome');
   }
 }
